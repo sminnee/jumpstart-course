10bf54d870654e775a20459c1c09d192dac7c937
Cam Findlay <cam@silverstripe.com>
2015-02-25 17:28:26 +1300

    1.3 Setting up environment configuration file

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,9 +4,21 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = '';
+$database = 'SS_mysite';
 
 require_once('conf/ConfigureFromEnv.php');
+
+if(SS_ENVIRONMENT_TYPE == 'dev'){ //Only use SQLite if in development mode
+
+	global $databaseConfig;
+	$databaseConfig = array(
+		"type" => 'SQLiteDatabase',
+		"database" => $database,
+		"path" => 'assets/.db'
+	);
+
+}
+
 
 // Set the site locale
 i18n::set_locale('en_US');
